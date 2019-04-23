10d17d1d730a9525cf39fbeeac99870bc1069d24
Antonio Trapani <antonio.trapani@gmail.com>
2019-04-23 13:11:34 +0200

    Lambda warm up

--- handler.js
+++ handler.js
@@ -1,4 +1,9 @@
-module.exports.hello = async () => {
+module.exports.hello = async (event) => {
+  if (event.source === 'serverless-plugin-warmup') {
+    console.log('WarmUp - Lambda is warm!')
+    return 'Warm Hello World!'
+  }
+
   return {
     statusCode: 200,
     body: `Frozen Hello World!`
